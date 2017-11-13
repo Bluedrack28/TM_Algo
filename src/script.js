84d50394105cbb75961002442df8c04b4c8e25b8
Bluedrack28 <bluedrack28@gmail.com>
2017-11-13 21:04:21 +0100

    Quelques modifs de test.

--- src/script.js
+++ src/script.js
@@ -5,14 +5,14 @@ let objet1 = new RoundObjet(100, 100, 100, 10);
 let objet2 = new RoundObjet(200, 100, 100, 10);
 let objet3 = new RoundObjet(100, 200, 100, 10);
 let objet4 = new RoundObjet(100, 200, 100, 10);
-objet1.addForce(new Force(0,+9.81));
-objet2.addForce(new Force(0,+9.81));
-objet3.addForce(new Force(0,+9.81));
-objet4.addForce(new Force(0,+9.81));
-let muscle = new Muscle(objet1, objet2, [new Motion(0.1,6),new Motion(0.3,3)],40,100);
-let muscle1 = new Muscle(objet1, objet3, [new Motion(0.2,6),new Motion(2,3)],20,50);
-let muscle2 = new Muscle(objet2, objet3, [new Motion(0.4,6),new Motion(0.3,3)],20,70);
-let muscle3 = new Muscle(objet1, objet4, [new Motion(0.4,6),new Motion(1,3)],40,100);
+objet1.addForce(new Force(0,98.1));
+objet2.addForce(new Force(0,98.1));
+objet3.addForce(new Force(0,98.1));
+//objet4.addForce(new Force(0,+9.81));
+let muscle = new Muscle(objet1, objet2, [new Motion(Math.random()*10,Math.random()*10),new Motion(Math.random()*10,Math.random()*10)],Math.random()*100,Math.random()*100);
+let muscle1 = new Muscle(objet1, objet3, [new Motion(Math.random()*10,Math.random()*10),new Motion(Math.random()*10,Math.random()*10)],Math.random()*100,Math.random()*100);
+let muscle2 = new Muscle(objet2, objet3, [new Motion(Math.random()*10,Math.random()*10),new Motion(Math.random()*10,Math.random()*10)],Math.random()*100,Math.random()*100);
+//let muscle3 = new Muscle(objet1, objet4, [new Motion(Math.random()*10,Math.random()*10),new Motion(Math.random()*10,Math.random()*10)],Math.random()*100,Math.random()*100);
 function update() {
     muscle.setForcesToObjets();
     muscle1.setForcesToObjets();
@@ -30,7 +30,6 @@ function draw() {
     drawVector(objet1,objet2);
     drawVector(objet2,objet3);
     drawVector(objet1,objet3);
-    drawVector(objet1,objet4);
     ctx.beginPath();
     
     ctx.fillStyle = 'grey'
@@ -44,11 +43,6 @@ function draw() {
     ctx.fillStyle = 'green'
     ctx.arc(objet3.x, objet3.y, objet3.raduis, 0, 2 * Math.PI);
     ctx.fill();
-    ctx.beginPath();
-    ctx.fillStyle = 'red'
-    ctx.arc(objet4.x, objet4.y, objet4.raduis, 0, 2 * Math.PI);
-    ctx.fill();
-
 
 }
 
